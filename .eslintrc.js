b39fb142aa29705fe069229e77d452ed5dcef13f
JounQin <admin@1stg.me>
2019-09-10 11:49:37 +0800

    fix(deps): bump dependencies, simplify build usage

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,4 @@
 const { overrides } = require('@1stg/eslint-config/overrides')
-const tsNode = require('ts-node')
-
-tsNode.register({
-  transpileOnly: true,
-  compilerOptions: {
-    module: 'commonjs',
-  },
-})
 
 module.exports = {
   extends: '@1stg',
